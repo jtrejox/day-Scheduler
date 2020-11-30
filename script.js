5de2a89534d2076dd885862e05b8963f26c5f2dd
Jorge Trejo <jtrejo@mail.com>
2020-11-30 02:46:26 -0800

    added save functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//VARIABLES
 var hours = ["12:00am", "1:00am", "2:00am", "3:00am", "4:00am", "5:00am", "6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am",
             "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm", "8:00pm", "9:00pm", "10:00pm", "11:00pm"]
 
@@ -7,24 +8,19 @@ var thFind = $('th')
 var middleFind = thFind.next('td')
 var lastFind = thFind.next('td').next()
 var mHour =moment().format("h:00a")
-var idx
+//SETTING THE JUMBOTRON DAY INFORMATION
 jumboPar.text(jumboDay)
 
-
-function fillDay(){                            //https://learn.jquery.com/using-jquery-core/iterating/
-    // var thFind = $('th').toArray()  //different version
-    // thFind.forEach(element => {  //different version
-    //     var i = thFind.indexOf(element)     //different version
-    //     element.append(hours[i])     //different version
-    //  }) //different version
-      $.each(thFind, (i, value) => {
+//FUNCTION TO POPULATE THE TIME CELLS
+function fillDay(){                            
+         $.each(thFind, (i, value) => {
         thFind[i].append(hours[i])
     });
 }
 
+//FUNCTION TO ADD ATTRIBUTES TO THE VARIOUS ELEMENTS
 function elementClass () {
     $.each(thFind, (i, e) =>{
-        idx = i
     thFind.attr('class', 'text-dark bg-light');
     if (i < hours.indexOf(mHour)){
         middleFind.eq(i).attr('class', 'middle bg-secondary')
@@ -35,27 +31,36 @@ function elementClass () {
     else
     {
             middleFind.eq(i).attr('class', 'middle bg-success')
-
     }
     middleFind.eq(i).attr('id', 'textbox'+i)
+    middleFind.eq(i).attr('data-saved', 'save'+i)
     lastFind.attr('class', 'last bg-primary')
     lastFind.eq(i).attr('id', 'save'+i)
+    lastFind.eq(i).attr('data-input', 'textbox'+i)
+    lastFind.eq(i).children().attr('data-input', 'textbox' + i)
+    lastFind.eq(i).children().children().attr('data-input', 'textbox' + i)
     lastFind.children().attr('class', 'mt-4 text-center')
-    // middleFind.attr('class', 'middle bg-success')
-      
-    // console.log(moment().format("h:00a"))
-    // console.log(mHour)
-    // console.log(hours.indexOf(mHour))
-    // console.log(i)
-    // console.log(thFind.text())
-  
     })
 }
 
+//FUNCTION TO RETRIEVE THE SAVED INFORMATION AND POPULATE IT INTO THE CELLS
+function savedStuff(){
+    $.each(middleFind, (i, v) =>{
+        var saved = middleFind.eq(i).attr('data-saved')
+        middleFind.eq(i).append(localStorage.getItem(saved))
+    })
+}
+
+//CALLING THE FUNCTIONS
 fillDay()
 elementClass()
+savedStuff()
 
+
+//EVENT LISTENER FOR THE SAVE BUTTON
 lastFind.on("click", function(target){
-    console.log($(target).closest('tr').children('textarea').value())//.first('tr'))
-
+    var info = target.currentTarget.getAttribute('id') 
+    var save = target.currentTarget.getAttribute('data-input')
+    var element = $('#'+save).children().children()[0]
+    localStorage.setItem(info, element.value)
 })
